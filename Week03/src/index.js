3b2a16577467aee5872623a5e17f6d561a0185eb
zyecastle <nesong722@gmail.com>
2025-05-04 23:37:11 +0900

    week05

--- Week03/src/index.js
+++ Week03/src/index.js
@@ -1,10 +1,21 @@
+import dotenv from "dotenv";
+import cors from "cors";
 import express from "express";
+
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello World! GIFIFIFIFI");
 });
+
+app.post("/api/v1/users/signup", handleUserSignup);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
